89d869cfd0bef21ced856cc70fe9e1b3e22832f8
madhurichalam <madhuri.chalam@so.energy>
2023-02-06 22:56:11 +0000

    FirstCommit

---
+++ src/Test/ReverseInteger.java
@@ -0,0 +1,25 @@
+package Test;
+
+public class ReverseInteger {
+
+    public static void main(String[] args)
+    {
+        //Using algo
+        int num = 12345;
+//        int rev = 0;
+//
+//        while(num != 0)
+//        {
+//            rev = (rev * 10) + (num % 10);
+//            num = num / 10;
+//
+//        }
+//        System.out.println("Reverse of the interger is : " + rev);
+
+        //Stringbuffer Object
+
+        System.out.println("Reverse of the interger is : " + new StringBuffer(String.valueOf(num)).reverse());
+
+
+    }
+}
